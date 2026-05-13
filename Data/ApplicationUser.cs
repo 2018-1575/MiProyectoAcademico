0e11e6ac90d9f404a44e464e3c007fdbaf316136
Yaniry <santoschama900@gmail.com>
2026-05-12 20:37:34 -0400

    Primer commit

---
+++ Data/ApplicationUser.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace MiProyectoAcademico.Data;
+
+// Add profile data for application users by adding properties to the ApplicationUser class
+public class ApplicationUser : IdentityUser
+{
+}
+
